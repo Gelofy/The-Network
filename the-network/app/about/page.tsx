313175958055b022b755a66124bbd13ea8bfac91
Gelofy <alfredgrondrup@gmail.com>
2023-05-19 23:44:16 +0200

    Initial commit

---
+++ the-network/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+        <div>AboutPage</div>
+        <Link href='/about/team'>Team</Link>
+    </>
+  )
+}
+
+export default AboutPage
