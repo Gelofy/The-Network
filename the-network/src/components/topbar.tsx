9fad3238ebe80624bdc0606e311ce7bd9b58a3ba
Gelofy <alfredgrondrup@gmail.com>
2023-06-13 16:30:06 +0200

    update

---
+++ the-network/src/components/topbar.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Typography, Divider, IconButton, Stack, Button, List,ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import topbarStyle from "@/styles/topbar.module.css";
+
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
+import DnsIcon from '@mui/icons-material/Dns';
+import InfoIcon from '@mui/icons-material/Info';
+import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
+import AnnouncementRoundedIcon from '@mui/icons-material/AnnouncementRounded';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Topbar(){
+  return (
+    <div className={ topbarStyle.div }></div>
+  );
+}
+export default Topbar;
