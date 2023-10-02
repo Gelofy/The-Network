ec873064dcfeea15d87baa5db8a5992f2de34315
Gelofy <alfredgrondrup@gmail.com>
2023-10-02 21:33:00 +0200

    Started working on the homepage.

--- the-network/src/app/page.tsx
+++ the-network/src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import Image from 'next/image'
-import { IconButton, Stack, Button, List,ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { IconButton, Divider, Stack, Button, List,ListItem, ListItemButton, ListItemIcon, ListItemText, Card, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import StorageIcon from '@mui/icons-material/Storage';
 import React, { createContext, useContext, useState } from 'react';
 // Uncomment to use supabase
 //import '../scripts/SupabaseClient';
 
+import "@/scripts/animateTitle";
 import { ShowBars } from '@/scripts/showBars';
 
 const theme = createTheme({
@@ -15,6 +17,20 @@ const theme = createTheme({
       contrastText: "#FFF",
     },
   },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
 });
 
 export default function Home() {
@@ -23,7 +39,15 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <main className='basepage'>
-        <div></div>
+        <div className=''>
+          <Stack className="center" divider={<Divider orientation="horizontal" variant="middle" flexItem />} spacing={2}>
+            <StorageIcon className="text" sx={{ fontSize: "200px" }}></StorageIcon>
+            <Typography className="text" variant="h2" component="h1">
+              <b>The Network</b>
+            </Typography>
+          </Stack>
+          
+        </div>
 
       </main>
     </ThemeProvider>
